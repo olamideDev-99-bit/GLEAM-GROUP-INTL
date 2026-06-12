c6f29c88520700ddf174d58c2452ad03b7126195
JudeChukwuma14 <ebukajude14@gmail.com>
2026-06-12 16:16:54 +0100

    feat: export team member images from new assets index file

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,9 +1,9 @@
-import HR from './HR.jpg';
+import HR from './HR.JPG';
 import AssistantTL from "./AssistantTL.jpeg";
-import StrategicManager from "./StrategicManager.jpg";
+import StrategicManager from "./StrategicManager.JPG";
 import ProjectManager from "./ProjectManager.jpeg";
-import CeativeDirector from "./CeativeDirector.jpg";
-import TL from "./TLL.jpg";
+import CeativeDirector from "./CeativeDirector.JPG";
+import TL from "./TLL.JPG";
 import group from "./group.jpeg"
 
 export {
